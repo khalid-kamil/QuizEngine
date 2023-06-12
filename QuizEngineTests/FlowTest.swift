d2b4e4f743e7330cf009e36c4ab4c1e3bf9f7b83
Khalid Kamil <khalidkamil28@gmail.com>
2023-06-12 20:15:08 +0100

    Start and answer first question routes to second question. passing test.

--- QuizEngineTests/FlowTest.swift
+++ QuizEngineTests/FlowTest.swift
@@ -65,9 +65,19 @@ final class FlowTest: XCTestCase {
         XCTAssertEqual(router.routedQuestions, ["Q1", "Q1"])
     }
     
+    func test_startAndAnswerFirstQuestion_withTwoQuestions_routesToSecondQuestion() {
+        let router = RouterSpy()
+        let sut = Flow(questions: ["Q1", "Q2"], router: router)
+
+        sut.start()
+        router.answerCallback("A1")
+
+        XCTAssertEqual(router.routedQuestions, ["Q1", "Q2"])
+    }
+    
     class RouterSpy: Router {
         var routedQuestions = [String]()
-        var answerCallback: ((String) -> Void)? = nil
+        var answerCallback: ((String) -> Void) = { _ in }
         
         func routeTo(question: String, answerCallback: @escaping (String) -> Void) {
             routedQuestions.append(question)
